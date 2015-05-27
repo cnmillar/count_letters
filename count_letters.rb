baa95a8618881d60fc6485bb930812a29880e3e4
cnmillar <cnmillar@gmail.com>
2015-05-27 22:51:02 +0000

    Final edits to character counting exercise

--- count_letters.rb
+++ count_letters.rb
@@ -1,27 +1,43 @@
-# May 27, 2015
-# count_letters method
+# # May 27, 2015
+# # count_letters method
 
-# PART A
+# PART A - produces a method that counts the number of letters in a string
+
 def count_letters(string)
-	letters = {}
+	letters = Hash.new(0)
 	all_letters = string.delete(' ').split('')
-	all_letters.each do |i|
-		letters[i] == nil ? letters[i] = 1 : letters[i] += 1
-	end
+	all_letters.each { |i| letters[i] += 1 }
 	letters
 end
 
 puts count_letters("This is a string").inspect
 
-# PART B
-def count_letters(string)
-	letters = {}
-	all_letters = string.delete(' ').split('')
-	all_letters.each do |i|
-		letters[i] == nil ? letters[i] = 1 : letters[i] += 1
+# PART B - produces a method that tells you the element indices for each letter in a string
+
+def letter_indices(string)
+	letters = Hash.new { |h,k| h[k] = [] } # Why did I need to do this?
+	(0..string.length).each do |i|
+		if string[i] != " "
+			letters[string[i]] << i
+		end
 	end
 	letters
 end
 
-puts count_letters("This is a string").inspect
+puts letter_indices("This is a string").inspect
+
+# # PART B - attempt 1
+# def count_letters(string)
+# 	letters = Hash.new({})
+# 	all_letters = string.delete(' ').split('')
+# 	all_letters.each do |i|
+# 		letters[i][:count] == nil ? letters[i][:count] = 1 : letters[i][:count] += 1
+# 		letters[i][:index] = string.index(i) # only gives first occurance 
+# 	end
+# 	letters
+# end
+
+# puts count_letters("This is a string")
+
+
 
